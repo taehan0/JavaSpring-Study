b98ee185b3ffb1c400e7e29608c668fceccc9c17
taehan0 <taehan1000@naver.com>
2025-07-03 22:31:44 +0900

    first upload

---
+++ src/main/java/com/example/sbb/question/Question.java
@@ -0,0 +1,47 @@
+package com.example.sbb.question;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.sbb.answer.Answer;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import com.example.sbb.user.SiteUser;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Question
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+}
+
---
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.sbb.question.QuestionService;
+
+@SpringBootTest
+class SbbApplicationTests
+{
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    void testJpa()
+    {
+        for (int i = 0; i <= 300; i++)
+        {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content,null);
+        }
+    }
+}
